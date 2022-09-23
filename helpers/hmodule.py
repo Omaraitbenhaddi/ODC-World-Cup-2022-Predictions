062baf9c456f5b7f9d39b628b3879014a504bc12
yousra836 <jdainiyousra@gmail.com>
2022-09-23 10:41:43 +0100

    we added 2 functions 1.get_tier : to get our team tier and 2.marginal_effect : helps us to calculate the marginal effect

--- helpers/hmodule.py
+++ helpers/hmodule.py
@@ -19,6 +19,10 @@ def get_neutral(home_team, away_team):
 
 fr = pd.read_csv('../data/fifa_ranking.csv', sep=',', decimal='.')
 
+
+#--------------------------------------------------------------------------------------------
+
+
 def get_confederation(team):
     """the confederation of the team
 
@@ -30,3 +34,51 @@ def get_confederation(team):
     """
     conf = fr.loc[fr.countr_full==team, 'confederation'].unique()[0]
     return conf
+
+
+
+#-------------------------------------------------------------------------------------------
+
+
+
+def get_tier(team):
+    """_ the tier of the team
+
+    Args:
+        team (str): the team
+
+    Returns:
+        str: tier of the team
+    """
+    r = d_rank.loc[d_rank.country_full==team, 'rank'].iloc[0]
+    if r <= 8:
+        return 'diamond'
+    elif r <= 16:
+        return 'gold'
+    elif r <= 32:
+        return 'silver'
+    else:
+        return 'bronze'
+    
+    
+    
+#--------------------------------------------------------------------------------------------
+
+
+def marginal_effect(home_team, away_team):
+    """_ marginal effect
+
+    Args:
+        home_team (str): home team
+        away_team (str): away team
+    Returns:
+        float: marginal effect 
+    """
+    home_points = d_points.loc[d_points.country_full==home_team, 'total_points'].iloc[0]
+    away_points = d_points.loc[d_points.country_full==away_team, 'total_points'].iloc[0]
+    
+    return (home_points - away_points) / away_points
+
+
+#--------------------------------------------------------------------------------------------
+
